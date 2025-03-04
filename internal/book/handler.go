457fe3a698a57987a729edc01639aff7aa422fe8
Your Name <you@example.com>
2025-03-04 21:50:21 +0500

    parsing

--- internal/book/handler.go
+++ internal/book/handler.go
@@ -14,6 +14,12 @@ type BookResponse struct {
 	Price  float64 `json:"price"`
 }
 
+type Response struct {
+	Status  string      `json:"status"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data,omitempty"`
+}
+
 type Handler struct {
 	service *Service
 }
@@ -23,9 +29,12 @@ func NewHandler(service *Service) *Handler {
 }
 
 func (h *Handler) GetAllBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	books, err := h.service.GetAllBooks()
 	if err != nil {
-		http.Error(w, "Failed to retrieve books", http.StatusInternalServerError)
+		w.WriteHeader(http.StatusInternalServerError)
+		response := Response{Status: "fail", Message: "Failed to retrieve books"}
+		json.NewEncoder(w).Encode(response)
 		return
 	}
 
@@ -38,86 +47,110 @@ func (h *Handler) GetAllBooks(w http.ResponseWriter, r *http.Request) {
 			Price:  book.Price,
 		})
 	}
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(bookResponses)
+	w.WriteHeader(http.StatusOK)
+	response := Response{Status: "success", Message: "Books retrieved successfully"}
+	json.NewEncoder(w).Encode(response)
 }
 
 func (h *Handler) GetBookByID(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	vars := mux.Vars(r)
 	id, err := strconv.Atoi(vars["id"])
 	if err != nil {
-		http.Error(w, "Invalid book ID", http.StatusBadRequest)
+		w.WriteHeader(http.StatusBadRequest)
+		response := Response{Status: "fail", Message: "invalid book ID"}
+		json.NewEncoder(w).Encode(response)
 		return
 	}
 
 	book, err := h.service.GetBookByID(uint(id))
 	if err != nil {
-		http.Error(w, "Book not found", http.StatusNotFound)
+		w.WriteHeader(http.StatusNotFound)
+		response := Response{Status: "fail", Message: "Book not found"}
+		json.NewEncoder(w).Encode(response)
 		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(book)
+	w.WriteHeader(http.StatusOK)
+	response := Response{Status: "success", Message: "books retrieved by id successfully", Data: book}
+	json.NewEncoder(w).Encode(response)
 }
 
 func (h *Handler) CreateBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	var book Book
 	if err := json.NewDecoder(r.Body).Decode(&book); err != nil {
-		http.Error(w, "Invalid input", http.StatusBadRequest)
+		w.WriteHeader(http.StatusBadRequest)
+		response := Response{Status: "fail", Message: "Некорректное JSON-сообщение"}
+		json.NewEncoder(w).Encode(response)
 		return
 	}
 
 	// Создание книги с возвратом ID
 	if err := h.service.CreateBook(&book); err != nil {
-		http.Error(w, "Failed to create book", http.StatusInternalServerError)
+		w.WriteHeader(http.StatusInternalServerError)
+		response := Response{Status: "fail", Message: "Failed to create book"}
+		json.NewEncoder(w).Encode(response)
 		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(book)
+	response := Response{Status: "success", Message: "Book created successfully", Data: book}
+	json.NewEncoder(w).Encode(response)
 }
 
 func (h *Handler) UpdateBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	vars := mux.Vars(r)
 	id, err := strconv.Atoi(vars["id"])
 	if err != nil {
-		http.Error(w, "Invalid book ID", http.StatusBadRequest)
+		w.WriteHeader(http.StatusBadRequest)
+		response := Response{Status: "fail", Message: "Invalid book ID"}
+		json.NewEncoder(w).Encode(response)
 		return
 	}
 
 	var book Book
 	if err := json.NewDecoder(r.Body).Decode(&book); err != nil {
-		http.Error(w, "Invalid input", http.StatusBadRequest)
+		w.WriteHeader(http.StatusBadRequest)
+		response := Response{Status: "fail", Message: "Invalid Input"}
+		json.NewEncoder(w).Encode(response)
 		return
 	}
 
 	book.ID = uint(id)
 
 	if err := h.service.UpdateBook(book); err != nil {
-		http.Error(w, "Failed to update book", http.StatusInternalServerError)
+		w.WriteHeader(http.StatusInternalServerError)
+		response := Response{Status: "fail", Message: "Failed to update book"}
+		json.NewEncoder(w).Encode(response)
 		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(map[string]string{"message": "Book updated successfully"})
+	response := Response{Status: "success", Message: "Book updated successfully", Data: book}
+	json.NewEncoder(w).Encode(response)
 }
 
 func (h *Handler) DeleteBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	vars := mux.Vars(r)
 	id, err := strconv.Atoi(vars["id"])
 	if err != nil {
-		http.Error(w, "Invalid book ID", http.StatusBadRequest)
+		w.WriteHeader(http.StatusBadRequest)
+		response := Response{Status: "fail", Message: "Invalid book ID"}
+		json.NewEncoder(w).Encode(response)
 		return
 	}
 
 	if err := h.service.DeleteBook(uint(id)); err != nil {
-		http.Error(w, "Failed to delete book", http.StatusInternalServerError)
+		w.WriteHeader(http.StatusInternalServerError)
+		response := Response{Status: "fail", Message: "Failed to delete book"}
+		json.NewEncoder(w).Encode(response)
 		return
 	}
 
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(map[string]string{"message": "Book deleted successfully"})
+	response := Response{Status: "success", Message: "Book deleted successfully"}
+	json.NewEncoder(w).Encode(response)
 }
